5961693b714bf59416d8eb542103639ddffe77a4
zhangxy <zhangxiying@kq300061.com>
2018-10-09 16:38:52 +0800

    链家爬虫更新

--- 爬虫项目/2.Scrapy爬取链家二手房信息/2/lianjia3_django/lianjia3/views.py
+++ 爬虫项目/2.Scrapy爬取链家二手房信息/2/lianjia3_django/lianjia3/views.py
@@ -3,13 +3,24 @@ from lianjia3 import forms
 
 from lianjia3.models import lianjia3
 from django.shortcuts import HttpResponseRedirect,Http404,HttpResponse,render_to_response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def table(request):
     # table_form = forms.SignupForm()
-    list = lianjia3.objects.all()
-    # housedelId_List = lianjia3.objects.values("housedelId")
+    limit = 5  # 每页显示的记录数
+    lists = lianjia3.objects.all()
+    paginator = Paginator(lists, limit)  # 实例化一个分页对象
 
+    page = request.GET.get('page')  # 获取页码
+    try:
+        lists = paginator.page(page)  # 获取某页对应的记录
+    except PageNotAnInteger:  # 如果页码不是个整数
+        lists = paginator.page(1)  # 取第一页的记录
+    except EmptyPage:  # 如果页码太大，没有相应的记录
+        lists = paginator.page(paginator.num_pages)  # 取最后一页的记录
+
+    return render_to_response('table.html', {'lists': lists})
     # return render(request,'table.html',{'form':table_form})
-    return render_to_response("table.html", locals())
+    # return render_to_response("table.html", locals())
 
